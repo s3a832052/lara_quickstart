312678bf1eaf4072b6a3cdd2fa1d0611f9c558c9
GuoTingRui <azsxdc890902@gmail.com>
2021-11-22 22:39:47 +0800

    初步建立三個路由：get /, post /task, delete /task/{task}

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Task;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::post('/task', function (Request $request) {
+    //
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
