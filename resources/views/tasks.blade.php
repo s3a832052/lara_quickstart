556585158c3a3720452406f4afb5b2d7f2f6c92a
GuoTingRui <azsxdc890902@gmail.com>
2021-11-22 23:12:32 +0800

    刪除任務:修改tasks視圖&修改第三個路由

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -61,6 +61,12 @@
 
                             <td>
                                 <!-- 代辦：刪除按鈕 -->
+                                <form action="/task/{{ $task->id }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                    <button>刪除任務</button>
+                                </form>
                             </td>
                         </tr>
                     @endforeach
